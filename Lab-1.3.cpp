b338093febed6ace6429eeb041d92fc881d8f319
TrttiTrttu <71984695+TrttiTrttu@users.noreply.github.com>
2020-09-27 21:44:34 +0300

    Add files via upload

---
+++ Lab-1.3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bitset>
+using std::cin;
+using std::cout;
+
+int main() {	
+	int num;
+	cout << "Enter number: ";
+	cin >> num;
+	cout << num << std::endl;
+	cout << "Binary: " << std::bitset<8>(num) << std::endl;
+	cout << "Hex: " << std::hex << num << std::endl;
+	cout << "Boolean: " << bool(num) << std::endl;
+	cout << "Char: " << char(num);
+	return 0;
+}
