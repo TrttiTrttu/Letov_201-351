c27f98b69455f018cad236650db0bc9e519c5585
TrttiTrttu <trtti.trttu@gmail.com>
2020-10-20 15:50:49 +0300

    Update Letov_Grigorii_201_351_lab3_2.cpp

--- Letov_Grigorii_201_351_lab3_2.cpp
+++ Letov_Grigorii_201_351_lab3_2.cpp
@@ -7,16 +7,19 @@ using std::cout;
 using std::swap;
 
 
-int even_num(int* arr, int i) {
+int even_num(int* arr, int i)
+{
 	int s = 0;
 	int buf = arr[i];
 	int counter = 0;
-	while (buf > 0) {
+	while (buf > 0) 
+	{
 		counter++;
 		buf /= 10;
 	}
 	buf = arr[i];
-	while (buf > 0) {
+	while (buf > 0) 
+	{
 		if (counter % 2 == 0)
 			s += buf % 10;
 		buf /= 10;
@@ -26,7 +29,8 @@ int even_num(int* arr, int i) {
 }
 
 
-void input_mas(int *&arr, int *&buf, int *len) {
+void input_mas(int *&arr, int *&buf, int *len) 
+{
 	if (arr != nullptr) delete[] arr;
 	if (buf != nullptr) delete[] buf;
 	cout << "enter length: ";
@@ -39,80 +43,28 @@ void input_mas(int *&arr, int *&buf, int *len) {
 }
 
 
-void print_mas(int* arr, const int len) {
+void print_mas(int* arr, const int len) 
+{
 	for (int i = 0; i < len; i++)
 		cout << arr[i] << '\t';
 	cout << std::endl;
 }
 
-void Merge_even(int* arr, int* buf, int left, int right)
+
+void Merge(int* arr, int* buf, int left, int right, bool iseven)
 {
+
 	int counter = 0;
 	int b1 = left;
 	int mid = left + (right - left) / 2;
 	int b2 = mid + 1;
 
-	while (b1 <= mid && b2 <= right) {
-		if (even_num(arr,b1) <= even_num(arr,b2)) {
-			buf[counter] = arr[b1];
-			b1++;
-		}
-		else {
-			buf[counter] = arr[b2];
-			b2++;
-		}
-		counter++;
-	}
-	while (b1 <= mid) {
-		buf[counter] = arr[b1];
-		b1++;
-		counter++;
-	}
-	while (b2 <= right) {
-		buf[counter] = arr[b2];
-		b2++;
-		counter++;
-	}
-	for (b1 = 0; b1 < counter; b1++)
-		arr[left + b1] = buf[b1];
-}
-
-void MergeSort_even(int* arr, int* buf, int left, int right, int len)
-{
-	if (left < right)
-		if (right - left == 1) {
-			if (even_num(arr, right) < even_num(arr, left)) {
-				swap(arr[left], arr[right]);
-				//print_mas(arr,len);
-			}
-		}
-		else {
-			MergeSort_even(arr, buf, left, left + (right - left) / 2, len);
-			MergeSort_even(arr, buf, left + (right - left) / 2 + 1, right, len);
-			Merge_even(arr, buf, left, right);
-		}
-}
-
-void Merge_last_num(int* arr, int* buf, int left, int right)
-{
-	int counter = 0;
-	int b1 = left;
-	int mid = left + (right - left) / 2;
-	int b2 = mid + 1;
-
-	while (b1 <= mid && b2 <= right) {
-		if (arr[b1] % 10 == arr[b2] % 10) {
-			if (arr[b1] < arr[b2]) {
-				buf[counter] = arr[b2];
-				b2++;
-			}
-			else {
-				buf[counter] = arr[b1];
-				b1++;
-			}
-		}
-		else {
-			if (arr[b1] % 10 <= arr[b2] % 10) {
+	while (b1 <= mid && b2 <= right) 
+	{
+		if (iseven) 
+		{
+			if (even_num(arr, b1) <= even_num(arr, b2)) 
+			{
 				buf[counter] = arr[b1];
 				b1++;
 			}
@@ -121,14 +73,43 @@ void Merge_last_num(int* arr, int* buf, int left, int right)
 				b2++;
 			}
 		}
+		else {
+			if (arr[b1] % 10 == arr[b2] % 10)
+			{
+				if (arr[b1] < arr[b2]) 
+				{
+					
+					buf[counter] = arr[b2];
+					b2++;
+				}
+				else 
+				{
+					buf[counter] = arr[b1];
+					b1++;
+				}
+			}
+			else {
+				if (arr[b1] % 10 <= arr[b2] % 10) 
+				{
+					buf[counter] = arr[b1];
+					b1++;
+				}
+				else {
+					buf[counter] = arr[b2];
+					b2++;
+				}
+			}
+		}
 		counter++;
 	}
-	while (b1 <= mid) {
+	while (b1 <= mid) 
+	{
 		buf[counter] = arr[b1];
 		b1++;
 		counter++;
 	}
-	while (b2 <= right) {
+	while (b2 <= right) 
+	{
 		buf[counter] = arr[b2];
 		b2++;
 		counter++;
@@ -138,23 +119,42 @@ void Merge_last_num(int* arr, int* buf, int left, int right)
 }
 
 
-void MergeSort_last_num(int* arr, int* buf, int left, int right, int len)
+
+void MergeSort(int* arr,int* buf, int left, int right, int len, bool iseven)
 {
 	if (left < right)
-		if (right - left == 1) {
-			if (arr[right] % 10 == arr[left] % 10) {
-				if (arr[right] < arr[left]) { swap(arr[left], arr[right]); }
+		if (right - left == 1) 
+			if (iseven)
+			{
+				if (even_num(arr, right) < even_num(arr, left))
+				{
+					swap(arr[left], arr[right]);
+					//print_mas(arr,len);
+				}
 			}
 			else {
-				if (arr[right] % 10 < arr[left] % 10) { swap(arr[left], arr[right]); }
+				if (arr[right] % 10 == arr[left] % 10)
+				{	
+					if (arr[right] > arr[left])
+					{
+						swap(arr[left], arr[right]);
+					}
+				}
+				else {
+					if (arr[right] % 10 < arr[left] % 10)
+					{
+						swap(arr[left], arr[right]);
+					}
+				}
 			}
-		}
-		else {
-			MergeSort_last_num(arr, buf, left, left + (right - left) / 2, len);
-			MergeSort_last_num(arr, buf, left + (right - left) / 2 + 1, right, len);
-			Merge_last_num(arr, buf, left, right);
-		}
+			else 
+			{
+				MergeSort(arr, buf, left, left + (right - left) / 2, len, iseven);
+				MergeSort(arr, buf, left + (right - left) / 2 + 1, right, len, iseven);
+				Merge(arr, buf, left, right, iseven);
+			}
 }
+
 
 int main() {
 	int *arr = nullptr;
@@ -174,11 +174,11 @@ int main() {
 			break;
 		}
 		case 3: {
-			MergeSort_even(arr, buf, 0, len - 1, len);
+			MergeSort(arr, buf, 0, len - 1, len, true);
 			break;
 		}
 		case 4: {
-			MergeSort_last_num(arr, buf, 0, len - 1, len);
+			MergeSort(arr, buf, 0, len - 1, len, false);
 			break;
 		}
 		case 5:
