eacc48b8bea064c90e353ae172208bb7fe2b7ca4
TrttiTrttu <trtti.trttu@gmail.com>
2020-10-29 01:06:58 +0300

    Update Letov_Grigorii_201_351_lab4_2.cpp

--- Letov_Grigorii_201_351_lab4_2.cpp
+++ Letov_Grigorii_201_351_lab4_2.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <Windows.h>
+
+using std::cout;
 
 int main()
 {
+	SetConsoleCP(1251); // функции из Windows.h 
+	SetConsoleOutputCP(1251);
+	for (int i = 0; i <= 255; i++)
+	{
+		cout << i << '\t' << char(i) << '\n';
+	}
 	
 }
