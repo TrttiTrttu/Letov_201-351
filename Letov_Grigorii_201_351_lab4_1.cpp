36abac5aa80568a7846b2158507db09ccfddb397
TrttiTrttu <trtti.trttu@gmail.com>
2020-11-07 11:39:35 +0300

    й

--- Letov_Grigorii_201_351_lab4_1.cpp
+++ Letov_Grigorii_201_351_lab4_1.cpp
@@ -12,20 +12,18 @@ void palindrom(char* str) // принимает указатель на строку
 	for (int l = 0, r = strlen(str) - 1; l < r; ) // до тех пор пока сдвиграемые границы не встретятся в середине
 		
 		if (int(str[l]) == int(str[r]) || abs(int(str[l]) - int(str[r])) == 32) // проверка на разные регистры и равенство по таблице ASCII 
-		{
-			r--; l++; // сдвиг счетчиков левой и правой границы
-		}
+		{ r--; l++; }// сдвиг счетчиков левой и правой границы
+		
 		else if (str[l] == ' ' && str[r] != ' ')  // если левая граница пробел
 			l++; // сдвиг левого счеттчика
 		
-		else if (str[l] != ' ' && str[r] == ' ') // если правый элемент пробед
+		else if (str[l] != ' ' && str[r] == ' ') // если правый элемент пробел
 			r--; // сдвиг правого счетчика
 
 		else // если элементы различаются
-		{
-			flag = false; break; // флаг на ложь и выход из цикла
-		}
-	cout << ((flag) ? "Палиндром" : "Не палиндром"); // тернарный оператор в зависости от значения флага
+		{ flag = false; break; }// флаг на ложь и выход из цикла
+
+	cout << ((flag) ? "Палиндром" : "Не палиндром"); // тернарный оператор в зависимости от значения флага
 }
 
 void cesar_code(char* str, int num) //принимает указатель на строку и константу ключа шифра
@@ -35,13 +33,16 @@ void cesar_code(char* str, int num) //принимает указатель на строку и константу 
 	for (int i = 0; i < strlen(str); i++) 
 	
 		if (str[i] != ' ') // если элемент не пробел
-		
-			if(int(str[i]) + num < int('z') + 1) // проверка что сдвиг не вылезает за границу алфавита
+		{
+			if (int(str[i]) + num < int('z') && str[i] + num > int('Z')) // проверка что сдвиг не вылезает за границу алфавита
 				str_for_encrypt[i] = char(int(int(str[i]) + num)); // изменение элемента по таблице ASCII путем явных преобразований
-			
-			else
-				str_for_encrypt[i] = char(int('a' + int(str[i]) + num - 'z' - 1)); // изменение если сдвиг вылазит за границу алфавита
-	
+			if (int(str[i]) + num > int('z') && str[i] + num > int('Z'))
+				str_for_encrypt[i] = char(int('a' + int(str[i]) + num - 'z' - 1)); 
+			if (int(str[i]) + num < int('z') && str[i] + num < int('Z')) 
+				str_for_encrypt[i] = char(int(int(str[i]) + num)); 
+			if (int(str[i]) + num < int('z') && str[i] + num > int('Z')) 
+				str_for_encrypt[i] = char(int('A' + int(str[i]) + num - 'Z' - 1));
+		}
 	str_for_encrypt[strlen(str)] = '\0'; // добавление нуля-терминатора в конце строки
 	cout << str_for_encrypt;
 }
@@ -60,7 +61,7 @@ void names(char* str)
 			i++; // увеличение индекса элемента чтобы пропустить кавычку
 			int j = 0; // отдельный счетчик для строки-буфера
 			
-			while (counter < 2)
+			while (counter < 2 && str[i] != '\0')
 			{
 				buf[j] = str[i]; // копирование "названия" в буфер
 				i++; // увеличение счетчика цикла for ( счетчик исходной строки )
@@ -79,12 +80,12 @@ void clear(char *str) // принимет указатель на строку
 		str[i] = '.'; // заполянет массив точками
 }
 
-bool* find(char* str, char* substr) // принимает указатели на строку и подстроку
+bool* find(char* str, char* substr, int pos) // принимает указатели на строку и подстроку
 {
 	char* buf = new char[strlen(str)]{}; // объявление буферной строки
 	bool* index_arr = new bool[strlen(str)]{ false };
 	
-	for (int i = 0; i < strlen(str); )
+	for (int i = pos; i < strlen(str); )
 	{
 		int counter = 0; // объявление счетчика
 		
@@ -114,8 +115,8 @@ bool* find(char* str, char* substr) // принимает указатели на строку и подстроку
 
 int main()
 {
-	SetConsoleCP(1251); // функции из Windows.h 
-	SetConsoleOutputCP(1251); // для локализации ввода\вывода консоли
+	SetConsoleCP(1251);			// функции из Windows.h 
+	SetConsoleOutputCP(1251);	// для локализации ввода\вывода консоли
 	char* pstr = new char[255]{}; // объявление пустой строки
 	char* psubstr = new char[255]{}; // объявление пустой подстроки для 2-го задания
 	bool* index = nullptr; // объявление пустого указателя для подсчета индексов вхождения
@@ -123,11 +124,11 @@ int main()
 	while (true)
 	{
 		cout << "choose your task:\n"
-			"1. palindrom\n"
-			"2. find substring\n"
-			"3. cesar code\n"
-			"4. \"names\"\n"
-			"5. exit\n";
+				"1. palindrom\n"
+				"2. find substring\n"
+				"3. cesar code\n"
+				"4. \"names\"\n"
+				"5. exit\n";
 		cin >> choice;
 		switch (choice)
 		{
@@ -147,7 +148,10 @@ int main()
 			cin.getline(pstr, 255);
 			cout << "Введите подстроку для поиска: \n";
 			cin.getline(psubstr, 255);
-			index = find(pstr, psubstr);
+			int pos = 0;
+			cout << "Введите стартовую позицию поиска: \n";
+			cin >> pos;
+			index = find(pstr, psubstr,pos);
 			for (int i = 0; i < strlen(pstr); i++)
 				if (index[i] != false)
 					cout << "index = " << i << '\n';
--- Letov_Grigorii_201_351_lab4_2.cpp
+++ Letov_Grigorii_201_351_lab4_2.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <Windows.h>
+#include <string>
 
 using std::cout;
+using std::cin;
+using std::string;
+using std::getline;
+
+string file_name(const string file_path_full)
+{
+	string rtrn;
+	rtrn.assign(file_path_full, file_path_full.rfind('/') + 1, (file_path_full.rfind('.') - file_path_full.rfind('/') - 1));
+	return rtrn;
+}
 
 int main()
 {
 	SetConsoleCP(1251); // функции из Windows.h 
-	SetConsoleOutputCP(1251);
-	for (int i = 0; i <= 255; i++)
-	{
-		cout << i << '\t' << char(i) << '\n';
-	}
-	
+	SetConsoleOutputCP(1251); // для локализации ввода\вывода консоли
+	string file;
+	getline(cin, file);
+	cout << file_name(file);
 }
